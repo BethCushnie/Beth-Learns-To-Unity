c44a983be11de36867df11aee7e1c77c958909aa
BethCushnie <github@bethcushnie.com>
2020-03-30 21:28:43 -0400

    simple character movement

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿// GetKey is true if the key is down during the frame
+// GetKeyDown is true only if this is the first frame where the key is down
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKeyDown(KeyCode.W))
+            position.x++;
+
+        transform.position = position;
+    }
+}
