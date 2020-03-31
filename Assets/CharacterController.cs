9e96772a4ea6b69f942f2b8c0b19e38fd8fc868c
BethCushnie <github@bethcushnie.com>
2020-03-30 21:42:35 -0400

    smoooooooth movement (also learned about floating points)

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,34 +1,20 @@
-﻿// GetKey is true if the key is down during the frame
-// GetKeyDown is true only if this is the first frame where the key is down
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float Speed;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 position = transform.position;
 
-        if (Input.GetKeyDown(KeyCode.W))
-            position.x++;
-
-        if (Input.GetKeyDown(KeyCode.S))
-            position.x--;
-
-        if (Input.GetKeyDown(KeyCode.A))
-            position.z++;
-
-        if (Input.GetKeyDown(KeyCode.D))
-            position.z--;
+        if (Input.GetKey(KeyCode.W))
+            position.x += Speed * Time.deltaTime;
+        // Because different computers have different frame rates, to get a consistent speed, multiple the general speed (set in unity) by Time.deltaTime
+        // which is basically just 1 / fps
 
         transform.position = position;
     }
