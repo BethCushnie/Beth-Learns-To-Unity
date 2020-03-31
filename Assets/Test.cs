25bf276c1f1b795fa603960841747d846334bb9f
BethCushnie <github@bethcushnie.com>
2020-03-30 20:53:30 -0400

    made the cube say "coding is fun"

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Test : MonoBehaviour
+{
+    public TextMeshPro childText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        childText.text = "coding is fun";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
