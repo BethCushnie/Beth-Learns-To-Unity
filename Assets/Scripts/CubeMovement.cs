c12035f452a4c480adfa3d845456c53b8fca5ff8
BethCushnie <github@bethcushnie.com>
2020-04-22 23:26:51 -0400

    better constant velocity code

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -16,16 +16,6 @@ public class CubeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = GetMovementVector();
-        transform.position += movement;
-    }
-
-    Vector3 GetMovementVector()
-    {
-        Vector3 movement = Vector3.forward;
-        //shorthand for = new Vector3(0, 0, 1);
-        //which is shorthand for = new Vector3; movement++
-        
-        return movement;
+        transform.position = Vector3.forward;
     }
 }
