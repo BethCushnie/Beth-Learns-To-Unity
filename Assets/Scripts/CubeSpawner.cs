51b5f8a81c1933ad2b6eccec8208e668450f54c1
BethCushnie <github@bethcushnie.com>
2020-04-23 00:14:33 -0400

    learned about instantiating prefabs

---
+++ Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject CubePrefab;
+
+    void Start()
+    {
+        GameObject cubeObject = Instantiate(CubePrefab);
+
+        cubeObject.transform.position = new Vector3(0, 0, 0);
+
+        CubeMovement cubeMovement = cubeObject.GetComponent<CubeMovement>();
+        cubeMovement.Speed = 3;
+        cubeMovement.MaxHeight = 10;
+    }
+
+    void Update()
+    {
+        
+    }
+}
