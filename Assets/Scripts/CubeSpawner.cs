7c1e2fcc7916907f1bd4108b898238035c5f18fc
BethCushnie <github@bethcushnie.com>
2020-04-23 23:21:00 -0400

    lists and loops

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -7,6 +7,9 @@ public class CubeSpawner : MonoBehaviour
     public GameObject CubePrefab;
     public int NumberOfCubes;
 
+
+    public List<CubeMovement> AllMovingCubes = new List<CubeMovement>();
+
     void Start()
     {
         for (int i = 0; i < NumberOfCubes; i++)
@@ -17,10 +20,30 @@ public class CubeSpawner : MonoBehaviour
 
             CubeMovement cubeMovement = cubeObject.GetComponent<CubeMovement>();
             cubeMovement.MovementDelay = i;
+
+            AllMovingCubes.Add(cubeMovement);
         }
     }
 
-    void Update()
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+            LoopOverCubes();
     }
+
+    void LoopOverCubes()
+    {
+        for (int i = 0; i < AllMovingCubes.Count; i++)
+        {
+            CubeMovement cubeMovement = AllMovingCubes[i];
+
+            cubeMovement.Amplitude++;
+        }
+    }
+
+    // think of <> as "of", and [] as "at"
+
+    // A list is a collection of many different objects of the same type. In this case, it lists all the cubes that are created.
+    // It also means that you can now affect that list all together. 
+    // LoopOverCubes is a loop function thing. It loops over the entire list, so you can set the properties of all the cubes at once.
 }
